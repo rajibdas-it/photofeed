51b615729c4712a7c5b8cea43f0aa53c21b62084
rajib <rajibrad@gmail.com>
2024-10-24 13:30:05 +0600

    write a middleware func for change url with locale

--- app/api/photos/route.js
+++ app/api/photos/route.js
@@ -1,7 +1,9 @@
 import { getAllPhotos } from "@/lib/image-data";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
+    // console.log(request.headers.get('accept-language'));
+    console.log(request.URL);
     const data = await getAllPhotos()
     return NextResponse.json(data)
 }
---
+++ middleware.js
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+let locales = ['bn', 'en']
+let defaultLocale = 'en'
+
+function getLocale(request) {
+    //browser theke ki language ashche
+    const acceptedLang = request.headers.get('accept-language') ?? undefined
+
+    let headers = { 'accept-language': acceptedLang }
+    let languages = new Negotiator({ headers }).languages()
+
+    return match(languages, locales, defaultLocale) //ei match function 3 parameter receive kore and then locale return kore.
+
+}
+
+
+export function middleware(request) {
+    const pathName = request.nextUrl.pathName  //pathname: '/api/photos'
+
+    const pathNameIsMissingLocale = locales.every((locale) => !pathName.startsWith(`/${locale}/`) && pathName !== `/${locale}`)
+
+    if (pathNameIsMissingLocale) {
+        const locale = getLocale(request)
+
+        return NextResponse.redirect(new URL(`/${locale}/${pathName}`, request.url))
+    }
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: [
+        // Skip all internal paths (_next, assets, api)
+        '/((?!api|assets|.*\\..*|_next).*)',
+        // Optional: only run on root (/) URL
+        // '/'
+    ],
+}
+
