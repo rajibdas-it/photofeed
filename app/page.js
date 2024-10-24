5cc4ea26a4b6f0c4ac6bf78a88449851a7572610
rajib <rajibrad@gmail.com>
2024-10-24 12:54:17 +0600

    set int-localmatcher and negotiator

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
-import { getAllPhotos } from "@/lib/image-data";
-import Image from "next/image";
 
 export default function Home() {
-  const photos = getAllPhotos()
+
 
   return (
     <h1>Photo Feed</h1>
